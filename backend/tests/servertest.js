529cfa0e3a18825d69b10a5caddda8a7867f996c
Jason Anderson <anderson.jason.az@gmail.com>
2022-01-25 11:17:58 -0700

    Add register tests to backend
    This helps ensure that the register api
    service is functioning properly

---
+++ backend/tests/servertest.js
@@ -0,0 +1,67 @@
+// Testing setup
+const axios = require("axios");
+const api = axios.create({
+    baseURL : "localhost:3000"
+});
+
+/************************** TESTING UTILITY METHODS ***************************/
+let reportFailure = function(message) {
+    console.error(message);
+}
+let assertNotNull = function(object) {
+    if (object == null) {
+        reportFailure("Response Object is Null!");
+    }
+}
+/******************************************************************************/
+
+
+/************************** REGISTER API TESTS ***************************/
+let registerTests = async function registerTests() {
+    let validRegisterRequest = {
+        username : "person123",
+        password : "password",
+        email : "randomemail@email.com"
+    }
+    
+    let invalidRegisterRequest = {
+        username : null,
+        password : "password",
+        email : null
+    }
+    
+    // Test Valid Register API call
+    try {
+        let response = await api.post("/api/user/register", validRegisterRequest);
+    
+        assertNotNull(response);
+    
+        if (!response.body.success) {
+            reportFailure("Register service failed when it should not have");
+        }
+        if (response.body.username !== validRegisterRequest.username) {
+            reportFailure("Response username does not match request username");
+        }
+    } catch (error) {
+        reportFailure(error)
+    }
+    
+    // Test Invalid Register API Call
+    try {
+        response = await api.post("/api/user/register", invalidRegisterRequest);
+        assertNotNull(response);
+    
+        if (response.body.success) {
+            reportFailure("Register service succeeded when it should not have");
+        }
+    } catch (error) {
+        reportFailure(error);
+    }
+}
+
+
+
+
+/***************************** Run Tests ********************************/
+// Comment out any tests that you do not need running
+registerTests();

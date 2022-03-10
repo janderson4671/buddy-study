b80bb6a78d740d04622759ecd7dba849f75c2d2a
Jason Anderson <anderson.jason.az@gmail.com>
2022-03-10 09:02:23 -0700

    Fix svelte node adapter issue

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -3,15 +3,13 @@ import adapter from '@sveltejs/adapter-node';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		}
+		adapter: adapter({
+			env: {
+				host: 'BUDDYSTUDY_HOST',
+				port: 'BUDDYSTUDY_PORT',
+				origin: 'BUDDYSTUDY_ORIGIN'
+			}
+		})
 	}
 };
 

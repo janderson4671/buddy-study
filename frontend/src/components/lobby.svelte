72516402c7b1b2c284ac2dd64826159b8142a831
joeparis6 <78243702+joeparis6@users.noreply.github.com>
2022-04-04 15:25:56 -0600

    "added components"

---
+++ frontend/src/components/lobby.svelte
@@ -0,0 +1,125 @@
+<script>
+
+    import { onMount } from "svelte";
+
+    onMount(async () => {
+        initializeGameVars();
+    });
+    
+    
+    const loadStudySets = async function() {
+        /*
+        $realtime = await Ably.Realtime({
+            authUrl: apiURL + "/api/game/auth"
+        });
+        $lobbyChannel = $realtime.channels.get(
+            `//${$lobbyId}:primary`
+        ); 
+        $hostAdminCh = $realtime.channels.get(
+            //`${$lobbyId}:host`
+        );
+        */
+    }
+
+    async function startGame() {
+        gameStarted = true;
+        
+	}
+
+    async function editStudySet() {
+        
+    }
+
+    
+</script>
+
+
+
+
+<!-- Should show the list of people in here -->
+<!-- <div class="guest_list">
+    {#each  as {}}
+        player.username 
+        {#if player.isReady}
+            Ready!
+        {/if}
+        {#if !player.isReady}
+            waiting...
+        {/if}
+    {/each}
+</div> -->
+
+<div class="buddyCode">
+    tempBuddy Code: sdf468w!
+
+    <p>Player1</p>
+    <p>Player2</p>
+</div>
+
+
+<div class="studysetTitle">
+    Geography
+
+    <!--Study Set: {$selectedStudySet.subject}-->
+</div>
+
+
+
+<!--Need to put this button inside of each study sets-->
+<div class="buttons">
+    <div class="startgame_button">
+        <button on:click={startGame}>Start game</button>
+    </div>
+
+    <div class="editStudySet_button">
+        <button on:click={editStudySet}>Change Study Set</button>
+    </div>
+</div>
+
+<style>
+
+    button {
+		border-radius: 30px;
+		font-family: 'Fira Sans Condensed', sans-serif;
+		font-style: normal;
+		font-weight: normal;
+		font-size: 1.2vw;
+		text-align: center;
+		background:#79C8F4;
+		color: black;
+		width: 10vw;
+		height: 3vw;
+		border: none;
+		box-shadow: 2px 3px gray;
+	}
+
+    .buttons{
+		display: flex;
+		justify-content: center;
+		margin-top: 3%;
+		margin-bottom: 2%;
+	}
+
+    .startgame_button {
+        margin-right: 2%;
+    }
+
+    .buddyCode {
+        text-align: center;
+        font-family: 'Fira Sans Condensed', sans-serif;
+        font-weight: bold;
+        margin: 3%;
+    }
+
+    .studysetTitle {
+        text-align: center;
+        font-family: 'Fira Sans Condensed', sans-serif;
+        font-weight: bold;
+        margin: 3%;
+    }
+	
+</style>
+	
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Fira+Sans+Condensed&display=swap" rel="stylesheet">
---
+++ frontend/src/components/question.svelte
@@ -0,0 +1,122 @@
+<script>
+
+    var qNum;
+    var qText = "Question";
+
+    var options = ["Answer 1", "Answer 2", "Answer 3", "Answer 4"];
+
+    var qTimer = 10;
+
+    var playerAnswer = "";
+    var correctAnswer = "";
+    var leaderboardTimer = 10;
+
+</script>
+
+    <div class="timer">
+        {leaderboardTimer}
+    </div>
+
+    <div class="game_question">
+        <h2>{qText}</h2>
+    </div>
+
+    <div class="game_answer">
+        <p>{options[0]}</p>
+    </div>
+
+    <div class="game_answer">
+        <p>{options[1]}</p>
+    </div>
+
+    <div class="game_answer">
+        <p>{options[2]}</p>
+    </div>
+
+    <div class="answer_selected">
+        <p>{options[3]}</p>
+    </div>
+
+    <div class="results_button" on:click={gotoResults}>
+       Results
+    </div>
+
+
+<style>
+
+    .timer {
+        text-align: center;
+        font-family: 'Fira Sans Condensed', sans-serif;
+		font-style: normal;
+		font-weight: normal;
+        font-size: 27px;
+        margin-bottom: 10px;
+    }
+
+    .game_question {
+        text-align: center;
+        display: flex;
+        margin-left: auto;
+        margin-right: auto;
+        margin-radius: auto;
+        margin-bottom: 5px;
+        justify-content: center;
+        background-color: #799BF4;
+        border-radius: 9px;
+        color: white;
+        width: 321px;
+        height: 104px;
+    }
+
+    .game_answer {
+        text-align: center;
+        display: flex;
+        margin-left: auto;
+        margin-right: auto;
+        margin-radius: auto;
+        margin-bottom: 4px;
+        border-radius: 9px;
+        justify-content: center;
+        background-color: #79c8f4;
+        width: 321px;
+    }
+
+    .answer_selected {
+        text-align: center;
+        display: flex;
+        margin-left: auto;
+        margin-right: auto;
+        margin-radius: auto;
+        margin-bottom: 4px;
+        border-radius: 9px;
+        border-style: solid;
+        border-color: black;
+        justify-content: center;
+        background-color: #79c8f4;
+        width: 321px;
+    }
+
+    .results_button {
+        margin-top: 3%;
+        border-radius: 30px;
+		font-family: 'Fira Sans Condensed', sans-serif;
+		font-style: normal;
+		font-weight: normal;
+		font-size: 1.2vw;
+		text-align: center;
+		background:#79C8F4;
+		color: black;
+		width: 10vw;
+		height: 3vw;
+		border: none;
+		box-shadow: 2px 3px gray;
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+</style>
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Fira+Sans+Condensed&display=swap" rel="stylesheet">

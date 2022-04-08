edcca1445fb84c7d37932657dce9d6e651be3d9f
joeparis6 <78243702+joeparis6@users.noreply.github.com>
2022-04-08 17:51:00 -0600

    changed exports

--- frontend/src/components/leaderboard.svelte
+++ frontend/src/components/leaderboard.svelte
@@ -6,36 +6,7 @@
 	import { goto } from "$app/navigation";
 	let startGameCode = null;
 
-    export let data = {
-		/* --- Host Base Variables --- */ 
-        hostAdminCh: null, 
-        lobbyReady: false, 
-        chosenStudySet: null, 
-
-        /* --- Normal Player Base Variables --- */ 
-        realtime: null, 
-        username: "host123", 
-        myClientId: null, 
-        globalChannelChName: "main-game-thread", 
-        globalChannel: null, 
-        lobbyId: null, 
-        lobbyChannel: null, 
-        myPlayerCh: null, 
-        players: {}, 
-        curStudySetName: null,
-        isReady: true,  
-        gameStarted: false, 
-        gameKilled: false, 
-
-        /* --- Component Flags --- */
-        isGuestJoin: true,
-        isCountdown: false,
-        isLobby: false,
-        isLeaderboard: false,
-        isQuestion: false,
-        isHost: false,
-        isSelectStudySet: false
-    }
+    export let data;
 
     var nextQuestionTimer = 10;
     var isLastQuestion = false;
--- frontend/src/components/question.svelte
+++ frontend/src/components/question.svelte
@@ -1,35 +1,6 @@
 <script>
 
-    export let data = {
-		/* --- Host Base Variables --- */ 
-        hostAdminCh: null:, 
-        lobbyReady: false:, 
-        chosenStudySet: null:, 
-
-        /* --- Normal Player Base Variables --- */ 
-        realtime: null:, 
-        username: "host123":, 
-        myClientId: null:, 
-        globalChannelChName: "main-game-thread":, 
-        globalChannel: null:, 
-        lobbyId: null:, 
-        lobbyChannel: null:, 
-        myPlayerCh: null:, 
-        players: {}:, 
-        curStudySetName: null:,
-        isReady: true:,  
-        gameStarted: false:, 
-        gameKilled: false:, 
-
-        /* --- Component Flags --- */
-        isGuestJoin: true:,
-        isCountdown: false:,
-        isLobby: false:,
-        isLeaderboard: false:,
-        isQuestion: false:,
-        isHost: false:,
-        isSelectStudySet: false
-    }
+    export let data;
 
     var qNum;
     var qText = "Question";
